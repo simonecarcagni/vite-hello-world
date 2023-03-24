2d2bf65f7d19f261d28147226925ddc5ba19a2b8
Simone Carcagnì <simonecarcagni@libero.it>
2023-03-24 17:08:04 +0100

    Esercizio Completato

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<template>
+    <div class="img-container">
+        <img src="../assets/01.webp" alt="SpiderMan">
+    </div>
+</template>
+
+<style scoped>
+.img-container {
+    height: 500px;
+    width: 800px;
+    margin: auto;
+}
+
+img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+h1 {
+    text-align: center;
+}
+
+html {
+    font-family: sans-serif;
+}
+</style>
